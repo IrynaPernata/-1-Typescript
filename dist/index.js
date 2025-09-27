bddb2722af77a6ae578431866b21723b8be159cf
IrynaPernata <pernata.iryna@gmail.com>
2025-09-27 12:24:38 +0300

    Практична робота №1 done

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let userName = "Ірина";
+let age = 21;
+let isStudent = true;
+console.log(`Привіт, мене звати ${userName}, мені ${age} років.`);
+console.log(`Чи я студентка? ${isStudent ? "Так" : "Ні"}`);
